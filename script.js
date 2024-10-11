ca0765827c6805234ea07842ede55213c33dc22a
Mark Gichora <markgichora7@gmail.com>
2024-10-11 22:43:02 +0300

    added form validation

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    const form = document.getElementById("registration-form");
+    const feedbackDiv = document.getElementById('form-feedback')
+
+    document.addEventListener("submit", function (event) {
+        event.preventDefault()
+        const username = document.getElementById("username").value.trim()
+        const email = document.getElementById("email").value.trim()
+        const password = document.getElementById("password").value.trim()
+
+        var isValid = true
+        var messages = []
+        if (username.length < 3) {
+            isValid = false
+            messages.push("Invalid Username. Characters should be more than 3");
+        }
+        if (!email.includes('@') || !email.includes('.')) {
+            isValid = false;
+            messages.push("Email must contain both '@' and '.'.");
+        }
+
+        if (password.length < 8) {
+            isValid = false;
+            messages.push("Password must be at least 8 characters long.");
+        }
+
+        feedbackDiv.style.display = "block";
+
+        if (isValid) {
+            feedbackDiv.textContent = "Registration successful!";
+            feedbackDiv.style.color = "#28a745"; 
+        } else {
+            feedbackDiv.innerHTML = messages.join("<br>");
+            feedbackDiv.style.color = "#dc3545"; 
+        }
+
+    })
+})
